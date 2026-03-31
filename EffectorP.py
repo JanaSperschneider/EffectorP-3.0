f014c408fe82589083b9255c02abdf5be8b913a0
Jana Sperschneider <jana.sperschneider@csiro.au>
2026-03-31 13:42:13 +1100

    Update EffectorP.py

--- EffectorP.py
+++ EffectorP.py
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    Contact: jana.sperschneider@anu.edu.au or jana.sperschneider@csiro.au
+    Contact: jana.sperschneider@csiro.au
 """
 # -----------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------
